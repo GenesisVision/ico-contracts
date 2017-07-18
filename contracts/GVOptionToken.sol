40d07a6de5ef19afd92f842355b5bc11f24b1537
r-kamenskiy <r@kamenskiy.net>
2017-07-18 15:04:51 +0300

    GV options workflow management added  (start, pause, finish)

--- contracts/GVOptionToken.sol
+++ contracts/GVOptionToken.sol
@@ -1,18 +1,50 @@
-pragma solidity ^0.4.12;
+pragma solidity ^0.4.11;
 
 // ERC20 interface is implemented only partially.
 
 contract GVOptionToken {
     
-    function GVOptionToken(address _tokenManager, address _escrow) {
-        tokenManager = tokenManager;
-        escrow = _escrow;
-    }
-
+    // Constants
     string public constant name = "Genesis Vision Option Token";
     string public constant symbol = "GVOT";
     uint   public constant decimals = 18;
 
-    address public tokenManager;
-    address public escrow;
+    // Events
+    event StartOptionsSelling();
+    event PauseOptionsSelling();
+    event FinishOptionsSelling();
+
+    // State variables
+    address public gvAgent; // payments bot account
+    address public team;    // team account
+
+    // Modifiers
+    modifier teamOnly { require(msg.sender == team); _; }
+    modifier gvAgentOnly { require(msg.sender == gvAgent); _; }
+
+    enum OptionsSellingState { Created, Running, Paused, Finished }
+    OptionsSellingState optionsSellingState = OptionsSellingState.Created;
+    
+    function GVOptionToken(address _gvAgent, address _team) {
+        gvAgent = _gvAgent;
+        team = _team;
+    }
+
+    function startOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Created || optionsSellingState == OptionsSellingState.Paused);
+        optionsSellingState = OptionsSellingState.Running;
+        StartOptionsSelling();
+    }
+
+    function pauseOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Running);
+        optionsSellingState = OptionsSellingState.Paused;
+        PauseOptionsSelling();
+    }
+
+    function finishOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Running || optionsSellingState == OptionsSellingState.Paused);
+        optionsSellingState = OptionsSellingState.Finished;
+        FinishOptionsSelling();
+    }    
 }
