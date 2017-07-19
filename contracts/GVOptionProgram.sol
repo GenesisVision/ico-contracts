2d9e815f9c76e6deca673994521ea2f7d3ca05e1
r-kamenskiy <r@kamenskiy.net>
2017-07-19 18:47:37 +0300

    GVOptionProgram added

---
+++ contracts/GVOptionProgram.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.11;
+
+import './GVOptionToken.sol';
+
+contract GVOptionProgram {
+
+    // Events
+    event StartOptionsSelling();
+    event PauseOptionsSelling();
+    event FinishOptionsSelling();
+
+    // State variables
+    address public gvAgent; // payments bot account
+    address public team;    // team account
+
+    GVOptionToken gvOptionToken;
+
+    // Modifiers
+    modifier teamOnly { require(msg.sender == team); _; }
+    modifier gvAgentOnly { require(msg.sender == gvAgent); _; }
+
+    enum OptionsSellingState { Created, Running, Paused, Finished }
+    OptionsSellingState optionsSellingState = OptionsSellingState.Created;
+    
+    function GVOptionProgram(address _gvAgent, address _team) {
+        gvOptionToken = new GVOptionToken(this);
+        gvAgent = _gvAgent;
+        team = _team;
+    }
+
+    function startOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Created || optionsSellingState == OptionsSellingState.Paused);
+        optionsSellingState = OptionsSellingState.Running;
+        StartOptionsSelling();
+    }
+
+    function pauseOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Running);
+        optionsSellingState = OptionsSellingState.Paused;
+        PauseOptionsSelling();
+    }
+
+    function finishOptionsSelling() external teamOnly {
+        require(optionsSellingState == OptionsSellingState.Running || optionsSellingState == OptionsSellingState.Paused);
+        optionsSellingState = OptionsSellingState.Finished;
+        FinishOptionsSelling();
+    }    
+}
--- contracts/GVOptionToken.sol
+++ contracts/GVOptionToken.sol
@@ -1,50 +1,29 @@
 pragma solidity ^0.4.11;
 
-// ERC20 interface is implemented only partially.
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
-contract GVOptionToken {
+contract GVOptionToken is StandardToken {
     
     // Constants
     string public constant name = "Genesis Vision Option Token";
     string public constant symbol = "GVOT";
     uint   public constant decimals = 18;
 
-    // Events
-    event StartOptionsSelling();
-    event PauseOptionsSelling();
-    event FinishOptionsSelling();
-
-    // State variables
-    address public gvAgent; // payments bot account
-    address public team;    // team account
-
-    // Modifiers
-    modifier teamOnly { require(msg.sender == team); _; }
-    modifier gvAgentOnly { require(msg.sender == gvAgent); _; }
-
-    enum OptionsSellingState { Created, Running, Paused, Finished }
-    OptionsSellingState optionsSellingState = OptionsSellingState.Created;
+    uint constant TOKEN_LIMIT = 1000; // TODO
+  
+    address public optionProgram;
     
-    function GVOptionToken(address _gvAgent, address _team) {
-        gvAgent = _gvAgent;
-        team = _team;
+    function GVOptionToken(address _optionProgram) {
+        optionProgram = _optionProgram;
     }
 
-    function startOptionsSelling() external teamOnly {
-        require(optionsSellingState == OptionsSellingState.Created || optionsSellingState == OptionsSellingState.Paused);
-        optionsSellingState = OptionsSellingState.Running;
-        StartOptionsSelling();
-    }
+    function buyOptions(address buyer, uint value) payable external {
+        require(msg.sender == optionProgram);
+        require(value > 0);
+        require(totalSupply + value <= TOKEN_LIMIT);
 
-    function pauseOptionsSelling() external teamOnly {
-        require(optionsSellingState == OptionsSellingState.Running);
-        optionsSellingState = OptionsSellingState.Paused;
-        PauseOptionsSelling();
+        balances[buyer] += value;
+        totalSupply += value;
+        Transfer(0x0, buyer, value);
     }
-
-    function finishOptionsSelling() external teamOnly {
-        require(optionsSellingState == OptionsSellingState.Running || optionsSellingState == OptionsSellingState.Paused);
-        optionsSellingState = OptionsSellingState.Finished;
-        FinishOptionsSelling();
-    }    
 }
