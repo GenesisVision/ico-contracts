83d723f0ddb66292d646ff1c1cefb2e77a5f16b2
r-kamenskiy <r@kamenskiy.net>
2017-07-27 19:34:40 +0300

    ICO tokens buying

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -8,28 +8,30 @@ contract ICO {
     // Constants
     uint public constant TOKEN_PRICE = 10; // GVT per 100 USD
     uint public constant TOKENS_FOR_SALE = 6000000 * 1e18; // TODO
-    uint public constant SNM_PER_SPT = 4; // Migration rate
 
     // Events
     event RunIco();
     event PauseIco();
     event FinishIco();
 
+    event BuyTokens(address buyer, uint amount, string txHash);
     // State variables
     address public gvAgent; // payments bot account
     address public team;    // team account
 
-    GVTToken gvtToken;
+    GVTToken public gvtToken;
     GVOptionProgram optionProgram;
 
     // Modifiers
     modifier teamOnly { require(msg.sender == team); _; }
     modifier gvAgentOnly { require(msg.sender == gvAgent); _; }
 
+    uint tokensSold = 0;
+
     enum IcoState { Created, Running, Paused, Finished }
     IcoState public icoState = IcoState.Created;
 
-    function ICO(address _gvAgent, address _team) {
+    function ICO( address _team, address _gvAgent) {
         gvtToken = new GVTToken(this);
         optionProgram = new GVOptionProgram(_gvAgent, _team);
         gvAgent = _gvAgent;
@@ -37,7 +39,6 @@ contract ICO {
     }
 
     function startIco() external teamOnly {
-        RunIco();
         require(icoState == IcoState.Created || icoState == IcoState.Paused);
         icoState = IcoState.Running;
         RunIco();
@@ -54,4 +55,16 @@ contract ICO {
         icoState = IcoState.Finished;
         FinishIco();
     }    
+
+    function buyTokens(address buyer, uint usdValue, string txHash)
+        external gvAgentOnly
+    {
+        require(icoState == IcoState.Running);
+        require(usdValue > 0);
+        uint tokens = usdValue * 1e17; // TODO check it
+        require(tokensSold + tokens <= TOKENS_FOR_SALE);
+
+        gvtToken.mint(buyer, tokens);
+        BuyTokens(buyer, tokens, txHash);
+    }
 }
---
+++ test/testtokenbuying.js
@@ -0,0 +1,56 @@
+var ICO = artifacts.require("./ICO.sol");
+var GVT = artifacts.require("./GVTToken.sol");
+
+contract('ICO', function (accounts) {
+    var account = accounts[0];
+    var account1 = accounts[1];
+    var account2 = accounts[2];
+    var account3 = accounts[3];
+    var account4 = accounts[4];
+
+    var ico;
+    var gvt; 
+
+    before('setup', (done) => {
+        ICO.deployed().then((_ico) => {
+            ico = _ico;
+            return ico.gvtToken.call();
+        })
+        .then((_gvt) => {
+            gvt = GVT.at(_gvt);
+            return ico.startIco()
+        })
+        .then(() => {
+            done();
+        });
+    });
+
+    it("should buy 1 token per 10 usd", () => {
+        return ico.buyTokens(account1, 10, "test")
+            .then(() => {
+                return gvt.balanceOf.call(account1)
+            })
+            .then((b) => {
+                assert.equal(1 * 1e18, b.valueOf(), "Balance should be 1");
+            });
+    });
+
+    it("should buy 5 token per 50 usd", () => {
+        return ico.buyTokens(account2, 50, "test")
+            .then(() => {
+                return gvt.balanceOf.call(account2)
+            })
+            .then((b) => {
+                assert.equal(5 * 1e18, b.valueOf(), "Balance should be 5");
+            });
+    });
+    it("should buy 101 token per 1010 usd", () => {
+        return ico.buyTokens(account3, 1010, "test")
+            .then(() => {
+                return gvt.balanceOf.call(account3)
+            })
+            .then((b) => {
+                assert.equal(101 * 1e18, b.valueOf(), "Balance should be 101");
+            });
+    });
+});
