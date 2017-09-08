aa35db4472648bcf2fae36ada4400ddd63d92707
Dmitry Nazarov <nazarov.dmitry.a@gmail.com>
2017-09-08 21:29:29 +0300

    Team allocator filled

--- contracts/GVTTeamAllocator.sol
+++ contracts/GVTTeamAllocator.sol
@@ -16,8 +16,19 @@ contract GVTTeamAllocator is Initable {
     function GVTTeamAllocator() {
         unlockedAt = now + 12 * 30 days;
         owner = msg.sender;
-        //ToDo Fill allocations table
-        //allocations[0x0] = 50; 50% of team tokens
+        
+        allocations[0xC2420A26DE68e097333222cbFe5FBbEE136c1DEC] = 38;
+        allocations[0xb205b75E932eC8B5582197052dB81830af372480] = 25;
+        allocations[0x48349271Cecd1788dC63c6Fc45bfDbF1B4Eb0E80] = 15;
+        allocations[0xeD44E60372AAE7DE227b00277C476dBAB260feF9] = 7;
+        allocations[0x36f3dAB9a9408Be0De81681eB5b50BAE53843Fe7] = 5; 
+        allocations[0x3dDc2592B66821eF93FF767cb7fF89c9E9C060C6] = 3; 
+        allocations[0xfD3eBadDD54cD61e37812438f60Fb9494CBBe0d4] = 2;
+        allocations[0xfE8B87Ae4fe6A565791B0cBD5418092eb2bE9647] = 2;
+        allocations[0x04FF8Fac2c0dD1EB5d28B0D7C111514055450dDC] = 1;           
+        allocations[0x1cd5B39373F52eEFffb5325cE4d51BCe3d1353f0] = 1;       
+        allocations[0xFA9930cbCd53c9779a079bdbE915b11905DfbEDE] = 1;        
+              
     }
 
     function init(address token) {
